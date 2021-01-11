32758fe875cad4f0675a12084945fa0b6447b85d
shanmukh101 <shanmukha.nooli@gmail.com>
2021-01-11 15:48:10 +0530

    initial_commit

---
+++ merge.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#define size 10
+
+
+void merge(int arr[], int beg, int mid, int end)
+{
+	int i=beg, j=mid+1, k=beg, temp[size],l;
+	while((i<=mid) && (j<=end))
+	{
+		 if(arr[i] <= arr[j])
+		 {
+			temp[k] = arr[i];
+			i++;
+		 }
+		 else
+		 {
+			temp[k] = arr[j];
+			j++;
+		 }
+		k++;
+	}
+		while(j<=end)
+		 {
+			temp[k] = arr[j];
+			j++;	
+			k++;
+		 }
+
+		 while(i<=mid)
+		 {
+			temp[k] = arr[i];
+			i++;
+			k++;
+		 }
+	
+	for(l=beg;l<k;l++)
+	arr[l] = temp[l];
+}	
+
+
+void merge_sort(int arr[], int beg, int end)
+{
+	int mid;
+	if(beg<end)
+	{
+		mid = (beg+end)/2;
+		 merge_sort(arr,beg, mid);
+		merge_sort(arr, mid+1, end);
+		 merge(arr, beg, mid, end);
+	}
+}
+
+void main()
+{
+	int arr[size], i, n=10;
+	printf("\n Enter the elements of the array: ");
+	for(i=0;i<n;i++)
+	{
+	 scanf("%d", &arr[i]);
+	}
+	merge_sort(arr, 0, n-1);
+	printf("\n The sorted array is: \n");
+	for(i=0;i<n;i++)
+		printf(" %d\t", arr[i]);
+}
+
+
