9a4a41b9103b9eb755c41df1e8b43162c0cc2dfe
shanmukh101 <shanmukha.nooli@gmail.com>
2021-01-12 18:53:20 +0530

    using dma calls

--- quick.c
+++ quick.c
@@ -1,48 +1,40 @@
-#include<stdio.h>
-void quicksort(int number[25],int first,int last){
-   int i, j, pivot, temp;
+int partition(int a[], int lb, int ub)
+{
 
-   if(first<last){
-      pivot=first;
-      i=first;
-      j=last;
-
-      while(i<j){
-         while(number[i]<=number[pivot]&&i<last)
-            i++;
-         while(number[j]>number[pivot])
-            j--;
-         if(i<j){
-            temp=number[i];
-            number[i]=number[j];
-            number[j]=temp;
-         }
-      }
-
-      temp=number[pivot];
-      number[pivot]=number[j];
-      number[j]=temp;
-      quicksort(number,first,j-1);
-      quicksort(number,j+1,last);
-
-   }
+	int pivot,start,end,temp;
+	pivot=a[lb];
+	start=lb;
+	end=ub;
+	while(start<end)
+	{
+		while(a[start]<=pivot)
+		{
+			start++;
+		}
+		while(a[end]>pivot)
+		{
+			end--;
+		}
+		if(start<end)
+		{
+			temp=a[start];
+			a[start]=a[end];
+			a[end]=temp;
+		}
+	}	
+	temp=a[lb];
+	a[lb]=a[end];
+	a[end]=temp;
+	return end;
+}
+void quick(int a[], int beg, int end)
+{
+	int loc;
+	if(beg<end)
+	{
+		 loc = partition(a, beg, end);
+		quick(a, beg, loc-1);
+		quick(a, loc+1, end);
+	}
 }
 
-int main(){
-   int i, count, number[25];
-
-   printf("How many elements are u going to enter?: ");
-   scanf("%d",&count);
-
-   printf("Enter %d elements: ", count);
-   for(i=0;i<count;i++)
-      scanf("%d",&number[i]);
-
-   quicksort(number,0,count-1);
-
-   printf("Order of Sorted elements: ");
-   for(i=0;i<count;i++)
-      printf(" %d",number[i]);
-
-   return 0;
-}
