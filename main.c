63df5fa9338bbcfbb805491e5ac14d0b18e2a199
shanmukh101 <shanmukha.nooli@gmail.com>
2021-01-12 20:25:43 +0530

    time calculation done

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"header.h"
+#include<time.h>
 
 
 void display(int arr[],int n)
@@ -13,7 +14,7 @@ void main(int argc,char *argv[])
 {
 	int n=argc-1,i=0;
 	int arr[n],arr_b[n],arr_m[n],arr_q[n],arr_s[n],arr_i[n];
-	
+	time_t start,end;
 	for(i=1;i<n+1;i++)
 	{
 	        arr[i-1]=atoi(argv[i]);
@@ -23,35 +24,50 @@ void main(int argc,char *argv[])
 		arr_s[i-1]=atoi(argv[i]);
                 arr_i[i-1]=atoi(argv[i]);
             }    
-          
-	printf("unsorted array is");
+        
+	printf("unsorted array is");	
 	display(arr,n);
 	printf("\n");
-		
-	bubble(arr_b,n);		
-	printf("sorted array is ");
-	display(arr_b,n);
-        printf("\n");	
-        
-        selection(arr_s,n);
-        printf("sorted array is");
-        display(arr_s,n);
-        printf("\n");
-        
-        insertion(arr_i,n);
-        printf("sorted array is ");
-        display(arr_i,n);
-        printf("\n");
+	printf("The time taken is %f seconds",difftime(end,start));
 	
+	start=time(NULL);	
+	bubble(arr_b,n);
+	end=time(NULL);		
+	printf("sorted array is ");	
+	display(arr_b,n);
+        printf("\n");
+        printf("The time taken is %f seconds",difftime(end,start));	
+        
+        start=time(NULL);
+        selection(arr_s,n);
+        end=time(NULL);
+        printf("sorted array is");
+        display(arr_s,n);        
+        printf("\n");
+        printf("The time taken is %f seconds",difftime(end,start));
+        
+        start=time(NULL);
+        insertion(arr_i,n);
+        end=time(NULL);
+        printf("sorted array is ");
+        display(arr_i,n);        
+        printf("\n");
+        printf("The time taken is %f seconds",difftime(end,start));
+	
+	start=time(NULL);
 	quick(arr_q,0,n-1);
+	end=time(NULL);
         printf("sorted array is ");
         display(arr_q,n);
         printf("\n");
+        printf("The time taken is %f seconds",difftime(end,start));
         
+        start=time(NULL);
         merge_sort(arr_m,0,n-1);
+        end=time(NULL);
         printf("sorted array is ");
         display(arr_m,n);
         printf("\n");
-	
+	printf("The time taken is %f seconds",difftime(end,start));
 }
 
